7252ae16918dac17f9e1b552b3c7b50d0c76a325
0918nobita <nobita.0918@gmail.com>
2020-11-03 00:34:20 +0900

    :sparkles: C++ 関数を CLR 側で呼び出し可能にするサンプルを追加

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Runtime.CompilerServices;
 
 public class Startup
 {
+    [MethodImpl(MethodImplOptions.InternalCall)]
+    extern static void CPPFunc();
+
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello from C#!");
+        CPPFunc();
     }
 }
--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 #include<mono/jit/jit.h>
 #include<mono/metadata/mono-config.h>
 
+void func() {
+    std::cout << "Hello from C++!" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
     // Load the default Mono configuration file
     mono_config_parse(NULL);
 
     // Create a domain: each assembly is loaded and run in a MonoDomain
     MonoDomain *domain = mono_jit_init("Startup.exe");
+
+    mono_add_internal_call("Startup::CPPFunc", (const void*) &func);
 
     // Open the executable, and run the Main method declared in the executable
     MonoAssembly *assembly = mono_domain_assembly_open(domain, "Startup.exe");
